2059f3e2cb332fd9fc77570d60b44ea5f0613219
shafidark <safi293172gmail.com>
2021-06-23 00:06:01 +0600

    shafi

--- src/Components/Home/User/User.js
+++ src/Components/Home/User/User.js
@@ -2,7 +2,8 @@ import React, { useContext, useState } from 'react';
 import './user.css';
 import UserForm from './UserForm';
 import {userContext} from '../../../App'
-const User = () => {
+const User = (props) => {
+    const {name, email, Role} = props.data
     const [click, setClick] = useState({
         clickToUser: true,
         clickToFrom:false
@@ -18,43 +19,16 @@ const User = () => {
         <div className='mt-5 text-center'>
         <h1 className='text-light'>User</h1>
           {click.clickToUser && <div>
-           <div className="clients d-flex justify-content-evenly mb-5">
-                <div className="name"><p>Ramos</p></div>
-                <select name="target" id="" className='selectProparty'>
-                    <option value="admin">Admin</option>
-                    <option value="oparetors">Oparetors</option>
-                    <option value="viewers">Viewers</option>
-                </select>
-                <button className="btn btn-primary btnArea">Edit</button>
-            </div>
+          
             <div className="clients d-flex justify-content-evenly mb-5">
-                <div className="name"><p>Ramos</p></div>
-                <select name="target" id="" className='selectProparty'>
+                <div className="name"><p>{name}</p></div>
+                <select name="target" id="" className='selectProparty' value={Role}>
                     <option value="admin">Admin</option>
                     <option value="oparetors">Oparetors</option>
                     <option value="viewers">Viewers</option>
                 </select>
                 <button className="btn btn-primary btnArea">Edit</button>
-            </div><div className="clients d-flex justify-content-evenly mb-5">
-                <div className="name"><p>Ramos</p></div>
-                <select name="target" id="" className='selectProparty'>
-                    <option value="admin">Admin</option>
-                    <option value="oparetors">Oparetors</option>
-                    <option value="viewers">Viewers</option>
-                </select>
-                <button className="btn btn-primary btnArea">Edit</button>
-            </div><div className="clients d-flex justify-content-evenly mb-5">
-                <div className="name"><p>Ramos</p></div>
-                <select name="target" id="" className='selectProparty'>
-                    <option value="admin">Admin</option>
-                    <option value="oparetors">Oparetors</option>
-                    <option value="viewers">Viewers</option>
-                </select>
-                <button className="btn btn-primary btnArea">Edit</button>
-            </div>
-            <div className="button d-flex justify-content-end">
-                <button className="btn btn-primary" onClick={handleClick}>Add User</button>
-            </div>    
+            </div> 
            </div>}
             <div>
                 {click.clickToFrom && <UserForm/>}
--- src/Components/Home/UserHome/UserHome.js
+++ src/Components/Home/UserHome/UserHome.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Navbar from '../../Shared/Navbar';
 import './UserHome.css'
 import User from '../User/User'
@@ -10,10 +10,33 @@ import Location from '../Locations/Locations';
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 
+if (!firebase.apps.length) {
+    firebase.initializeApp({
+        apiKey: "AIzaSyD_H-N97ETri2j8yI5c-X1YtguoBCWtWAU",
+        authDomain: "rlab-1f739.firebaseapp.com",
+        projectId: "rlab-1f739",
+        storageBucket: "rlab-1f739.appspot.com",
+        messagingSenderId: "663446234969",
+        appId: "1:663446234969:web:0cf0efc959e952732c8e48",
+        measurementId: "G-2EFCRDJGTP"
+    });
+ }else {
+    firebase.app(); // if already initialized, use that one
+ }
+
+const db = firebase.firestore();
 const UserHome = () => {
    
     const [user, setUser] = useContext(userContext);
-
+    const [dbUserData, setDbUserData] = useState([]);
+    useEffect(() => {
+        db.collection("user").get().then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                setDbUserData([...dbUserData,doc.data()]);
+            })
+        })
+    },[])
+    console.log(dbUserData);
     return (
         <div className='row'>
             
@@ -22,8 +45,9 @@ const UserHome = () => {
             </div>
             <div className="col-md-9 backgroundSIDE text-center">
             <img src={logo} alt="" style={{width:'20%'}} className='mt-5' />
+            <h1 className='text-light'>{dbUserData.length}</h1>
                 {user.userHome && <Rechart/>}
-                {user.user&& user.admin && <User/>}
+                {user.user&& user.admin && dbUserData.map(data => <User data={data}/>) }
                 {user.devices && <Devices/>}
                 {user.locations && <Location/>}
             </div>
