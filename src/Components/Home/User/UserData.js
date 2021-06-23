9cafefab14edb11e5cb3a18ece91fbd4e3d2bd2b
shafidark <safi293172gmail.com>
2021-06-23 17:19:10 +0600

    shafi

---
+++ src/Components/Home/User/UserData.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './user.css';
+const UserData = (props) => {
+    const {name, Role} = props.data
+    return (
+        <div>
+             <div className="clients d-flex justify-content-evenly mb-5">
+                <div className="name"><p>{name}</p></div>
+                <select name="target" id="" className='selectProparty' value={Role}>
+                    <option value="admin">Admin</option>
+                    <option value="oparetors">Oparetors</option>
+                    <option value="viewers">Viewers</option>
+                </select>
+                <button className="btn btn-primary btnArea">Edit</button>
+            </div> 
+        </div>
+    );
+};
+
+export default UserData;
