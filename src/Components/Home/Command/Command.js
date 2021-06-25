858efb0737cb34957ecd757b4e5225f7f7fecc94
shafidark <safi293172gmail.com>
2021-06-25 20:37:05 +0600

    shafi

--- src/Components/Home/Command/Command.js
+++ src/Components/Home/Command/Command.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 
+
 const Command = () => {
     const [pubSubM, setPubSubM] = useState({
         topic:'',
@@ -11,12 +12,15 @@ const handleOnChange = (event) => {
     newPubSubM[event.target.name] = event.target.value;
     setPubSubM(newPubSubM)
 }
+const handleSubmit = () => {
+    
+}
 console.log(pubSubM);
     return (
         <div>
             <h1 className='text-light'>Send command in Google Cloud Pub/sub</h1>
             <div>
-                <form action="">
+                <form action="" onSubmit={handleSubmit}>
                     <div className="d-flex justify-content-around">
                         <label htmlFor="topic">Topic</label>
                         <input type="text" placeholder="topic" name="topic" id="topic" onChange={handleOnChange}/>
@@ -29,7 +33,7 @@ console.log(pubSubM);
                         <label htmlFor="sub">Message</label>
                         <textarea placeholder="Message" id="message" name="message" onChange={handleOnChange}/>
                     </div>
-                    <button className="btn btn-primary">Send</button>
+                    <button className="btn btn-primary" type="submit" >Send</button>
                 </form>
             </div>
         </div>
