50d16534a4ed9e4a43e7c5f41f14d0162e122410
shafidark <67519541+shafidark@users.noreply.github.com>
2021-06-27 20:52:02 +0600

    raktim

--- src/Components/Home/Command/Command.js
+++ src/Components/Home/Command/Command.js
@@ -41,17 +41,17 @@ const handleSubmit = (e) => {
 }
 console.log(pubSubM);
     return (
-        <div className="command-container">
+        <div className="command-main-container">
             <h1 className='text-light command-title'>Send command in Google Cloud Pub/sub</h1>
-            <div>
+            <div className="command-container">
                 <form action="" onSubmit={handleSubmit}>
                     <div className="d-flex justify-content-end topic-container">
-                        <label className="command-text" htmlFor="topic">Device_id :</label>
+                        <label className="command-text" htmlFor="topic">Device_id:</label>
                         <input className="command-input" type="text" placeholder="Device_id" name="Device_id" id="topic" onChange={handleOnChange}/>
                     </div>
                     
                     <div className="d-flex justify-content-end topic-container">
-                        <label className="command-text" htmlFor="sub">Command :</label>
+                        <label className="command-text" htmlFor="sub">Command:</label>
                         <textarea className="command-input" placeholder="Command" id="Command" name="Command" onChange={handleOnChange}/>
                     </div>
                     <div className="text-end">
