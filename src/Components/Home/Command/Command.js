70d15c299b7f6080606ca632139205485cad523f
shafidark <safi293172gmail.com>
2021-06-27 15:50:36 +0600

    shafi

--- src/Components/Home/Command/Command.js
+++ src/Components/Home/Command/Command.js
@@ -1,18 +1,42 @@
 import React, { useState } from 'react';
+import firebase from 'firebase';
+import 'firebase/firestore';
+
+
+if (!firebase.apps.length) {
+    firebase.initializeApp({
+        apiKey: "AIzaSyA47b6Rx0RioZApSMcyDooUmOpQFFs9WLE",
+  authDomain: "test1-68872.firebaseapp.com",
+  projectId: "test1-68872",
+  storageBucket: "test1-68872.appspot.com",
+  messagingSenderId: "504703093399",
+  appId: "1:504703093399:web:50a89636d428ac8bd2f7d2"
+    });
+ }else {
+    firebase.app(); // if already initialized, use that one
+ }
+
+const db = firebase.firestore();
 
 const Command = () => {
     const [pubSubM, setPubSubM] = useState({
-        topic:'',
-        sub:'',
-        message:'',
+        Device_id:'',
+        Command:'',
     })
 const handleOnChange = (event) => {
     const newPubSubM = {...pubSubM};
     newPubSubM[event.target.name] = event.target.value;
     setPubSubM(newPubSubM)
 }
-const handleSubmit = () => {
-    
+const handleSubmit = (e) => {
+    db.collection("Command").add(pubSubM)
+    .then((docRef) => {
+        console.log("Document written with ID: ", docRef.id);
+    })
+    .catch((error) => {
+        console.error("Error adding document: ", error);
+    });
+    e.preventDefault();
 }
 console.log(pubSubM);
     return (
@@ -21,16 +45,13 @@ console.log(pubSubM);
             <div>
                 <form action="" onSubmit={handleSubmit}>
                     <div className="d-flex justify-content-around">
-                        <label htmlFor="topic">Topic</label>
-                        <input type="text" placeholder="topic" name="topic" id="topic" onChange={handleOnChange}/>
+                        <label htmlFor="topic">Device_id</label>
+                        <input type="text" placeholder="Device_id" name="Device_id" id="topic" onChange={handleOnChange}/>
                     </div>
+                    
                     <div className="d-flex justify-content-around">
-                        <label htmlFor="sub">Subscriptions</label>
-                        <input type="text" placeholder="Subscriptions" name="sub" id="sub" onChange={handleOnChange}/>
-                    </div>
-                    <div className="d-flex justify-content-around">
-                        <label htmlFor="sub">Message</label>
-                        <textarea placeholder="Message" id="message" name="message" onChange={handleOnChange}/>
+                        <label htmlFor="sub">Command</label>
+                        <textarea placeholder="Command" id="Command" name="Command" onChange={handleOnChange}/>
                     </div>
                     <button className="btn btn-primary" type="submit" >Send</button>
                 </form>
