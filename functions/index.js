d94415a3a98eccf17b7bc3d28a2b678df1f5f1f1
shafidark <safi293172gmail.com>
2021-06-28 12:43:32 +0600

    shafi

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,9 @@
 const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
 
 exports.helloPubSub = functions.pubsub.topic("Rlab12").onPublish((message) => {
-  // Get the `name` attribute of the PubSub message JSON body.
-  const messageBody = message.name?Buffer
-      .from(message.name, "base64").toString():null;
-  functions.logger.log(`Hello ${messageBody || "World"}!`);
-  return null;
+  const data = message.json;
+  console.log("Message is recived");
+  console.log(data);
 });
 
-exports.myFunction = functions.pubsub.topic("Rlab12").onPublish((context) => {
-  console.log("The function was triggered at ", context.timestamp);
-  console.log("The unique ID for the event is", context.eventId);
-});
