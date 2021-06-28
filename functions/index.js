3b3f4aa8b0d5608c821a8799dfe4085a2710479a
shafidark <safi293172gmail.com>
2021-06-28 16:21:26 +0600

    shafi

--- functions/index.js
+++ functions/index.js
@@ -3,9 +3,12 @@ const functions = require("firebase-functions");
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-
+const admin = require("firebase-admin");
+admin.initializeApp();
+const db = admin.firestore();
 exports.helloPubSub = functions.pubsub.topic("Rlab12").onPublish((message) => {
   const data = message.json;
+  db.collection("pubsub").add(data);
   console.log("Your pub sub is trigered");
   console.log(data);
 });
