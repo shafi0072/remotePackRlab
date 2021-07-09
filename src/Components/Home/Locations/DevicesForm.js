f14ecc9a72e287656f514dfb821cfde4bd2e7a57
shafidark <safi293172gmail.com>
2021-07-09 20:21:30 +0600

    shafi

--- src/Components/Home/Locations/DevicesForm.js
+++ src/Components/Home/Locations/DevicesForm.js
@@ -4,6 +4,7 @@ import firebase from 'firebase';
 import 'firebase/firestore';
 import DevicesData from './DevicesData';
 import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 if (!firebase.apps.length) {
     firebase.initializeApp({
         apiKey: "AIzaSyBXXL_2svIOIRYEArzq7Ai8G8xLQTTTqrU",
@@ -22,8 +23,11 @@ const db = firebase.firestore();
 
 const DevicesForm = (props) => {
     
-const {Device_id_1, Device_id_2, Device_id_3} = props.data
-    
+const {Device_id_1, Device_id_2, Device_id_3} = props.data;
+
+ const [device1, setDevice1] = useState({});
+ const [device2, setDevice2] = useState({});
+ const [device3, setDevice3] = useState({});
    
 console.log({Device_id_1});
 console.log({Device_id_2});
@@ -31,7 +35,7 @@ console.log({Device_id_3});
 useEffect(() => {
     db.collection("Devices").doc(Device_id_1).get().then((doc) => {
     if (doc.exists) {
-        console.log("Document data:", doc.data());
+        setDevice1(doc.data());
     } else {
         // doc.data() will be undefined in this case
         console.log("No such document!");
@@ -41,7 +45,7 @@ useEffect(() => {
 });
 db.collection("Devices").doc(Device_id_2).get().then((doc) => {
     if (doc.exists) {
-        console.log("Document data:", doc.data());
+        setDevice2(doc.data());
     } else {
         // doc.data() will be undefined in this case
         console.log("No such document!");
@@ -51,7 +55,7 @@ db.collection("Devices").doc(Device_id_2).get().then((doc) => {
 });
 db.collection("Devices").doc(Device_id_3).get().then((doc) => {
     if (doc.exists) {
-        console.log("Document data:", doc.data());
+        setDevice3(doc.data());
     } else {
         // doc.data() will be undefined in this case
         console.log("No such document!");
@@ -60,13 +64,31 @@ db.collection("Devices").doc(Device_id_3).get().then((doc) => {
     console.log("Error getting document:", error);
 });
 },[Device_id_1, Device_id_2, Device_id_3])
+console.log({device1});
+console.log({device2});
+console.log({device3});
     return (
             <div>
                <div className="container device-form-container">
                 
-                
-                  {/* <DevicesData />  */}
-                  
+               <div className="clients d-flex justify-content-evenly mb-5">
+                    <div className="form-name"><p className="form-name-p">{device1.model}</p></div>
+                     {/* <input type="text" placeholder='edit your Meter' name='one'/> */}
+                    <button className="btn btn-dark btnEdit" >Edit</button>
+                    <Link  className="btn btn-primary btnArea">Select</Link>
+                </div>
+                <div className="clients d-flex justify-content-evenly mb-5">
+                    <div className="form-name"><p className="form-name-p">{device2.model}</p></div>
+                     {/* <input type="text" placeholder='edit your Meter' name='one'/> */}
+                    <button className="btn btn-dark btnEdit" >Edit</button>
+                    <Link  className="btn btn-primary btnArea">Select</Link>
+                </div>
+                <div className="clients d-flex justify-content-evenly mb-5">
+                    <div className="form-name"><p className="form-name-p">{device3.model}</p></div>
+                     {/* <input type="text" placeholder='edit your Meter' name='one'/> */}
+                    <button className="btn btn-dark btnEdit" >Edit</button>
+                    <Link  className="btn btn-primary btnArea">Select</Link>
+                </div>
                 </div>
 
                 <div className="button d-flex justify-content-end deviceFrom-location-button">
