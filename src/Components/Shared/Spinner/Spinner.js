1ce8fe6356c7b8f8f7c3cc0540de994f16daf58f
shafidark <safi293172gmail.com>
2021-07-15 23:28:51 +0600

    shafi

--- src/Components/Shared/Spinner/Spinner.js
+++ src/Components/Shared/Spinner/Spinner.js
@@ -3,9 +3,21 @@ import React from 'react';
 const Spinner = () => {
     return (
         <div>
-            <div class="spinner-border" role="status">
+            <div class="spinner-grow text-dark " role="status">
                 <span class="visually-hidden">Loading...</span>
             </div>
+            <div class="spinner-grow text-dark ms-3" role="status">
+                <span class="visually-hidden">Loading...</span>
+            </div>
+            <div class="spinner-grow text-dark ms-3" role="status">
+                <span class="visually-hidden">Loading...</span>
+                
+            </div>
+            <div class="text-dark ms-3" role="status">
+            <p>Loading...</p>   
+                
+            </div>
+            
         </div>
     );
 };
